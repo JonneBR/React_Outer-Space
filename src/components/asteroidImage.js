a7e7f6977f9e1be1015625cd64c1c282a522c32e
jonnebr <joaomgoncalvesbr@gmail.com>
2020-12-22 06:03:52 -0300

    flex container and separating files

--- src/components/asteroidImage.js
+++ src/components/asteroidImage.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import asteroid from '../images/asteroid.svg'
+import './styles/asteroid.css'
 
 class Asteroid extends React.Component{
   render(){
     return(
-      <div>
-        <img width="350" src={asteroid} alt=""/>
+      <div class='asteroidImg'>
+        <img width="250" src={asteroid} alt=""/>
       </div>
     )
   }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './style.css'
+import './styles/style.css'
 
 class Navbar extends React.Component{
   render() {
